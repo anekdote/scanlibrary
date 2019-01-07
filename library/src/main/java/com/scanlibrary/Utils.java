4f3fe2de3a9867c31c5e5b74039dccda8286610a
anekdote <18257733+anekdote@users.noreply.github.com>
2019-01-07 10:53:42 -0800

    Update Utils.java

--- library/src/main/java/com/scanlibrary/Utils.java
+++ library/src/main/java/com/scanlibrary/Utils.java
@@ -2,8 +2,6 @@ package com.scanlibrary;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.Matrix;
-import android.graphics.RectF;
 import android.net.Uri;
 import android.provider.MediaStore;
 
@@ -28,18 +26,6 @@ public class Utils {
 
     public static Bitmap getBitmap(Context context, Uri uri) throws IOException {
         Bitmap bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), uri);
-        Bitmap scaled = scaleToSize(bitmap,2560);
-        return scaled;
-    }
-    
-    private static Bitmap scaleToSize(Bitmap bitmap,int size) {
-        Matrix m = new Matrix();
-        int originalWidth = bitmap.getWidth();
-        int originalHeight = bitmap.getHeight();
-        double ratio = originalWidth / originalHeight;
-        int newWidth = originalWidth > originalHeight ? size : (int)(size / ratio); 
-        int newHeight = originalWidth > originalHeight ? (int)(size/ratio) : size;
-        m.setRectToRect(new RectF(0, 0, bitmap.getWidth(), bitmap.getHeight()), new RectF(0, 0, newWidth, newHeight), Matrix.ScaleToFit.CENTER);
-        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), m, true);
+        return bitmap;
     }
 }
