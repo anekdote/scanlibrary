a80b0c2aceab86218f3173253a115c4a08c6c3bf
Byunghee Choi <byunghee.choi@kwitech.com>
2019-01-08 11:08:08 -0800

    ed

--- library/src/main/jni/scan.cpp
+++ library/src/main/jni/scan.cpp
@@ -80,7 +80,7 @@ __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "contours %d", contours.size()
                 // Note: absolute value of an area is used because
                 // area may be positive or negative - in accordance with the
                 // contour orientation
-                __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "contour size %d", fabs(contourArea(Mat(approx))));
+                __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "contour size %f", fabs(contourArea(Mat(approx))));
                 if (approx.size() == 4 &&
                     fabs(contourArea(Mat(approx))) > 1000 &&
                     isContourConvex(Mat(approx)))
